35cafa3770cb6fb56a4244c4db16e37fbb6d6d70
anssilap <anssilap@users.noreply.github.com>
2023-08-10 17:22:41 +0300

    Add files via upload
    Add premake file

---
+++ premake5.lua
@@ -0,0 +1,143 @@
+
+function get_glfw_dir()
+	return "glfw"
+end
+
+glfw_dir = get_glfw_dir();
+
+function link_glfw()
+	includedirs {"../" .. glfw_dir .. "/include" }
+
+	filter "action:vs*"
+		--defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS"}
+		dependson {"glfw"}
+		links {"GLFW.lib"}
+		--characterset ("MBCS")
+
+	filter "system:windows"
+		defines{"_WIN32"}
+		links {"winmm", "kernel32", "opengl32", "gdi32"}
+		libdirs {"../_bin/%{cfg.buildcfg}"}
+
+	-- filter "system:linux"
+	--     links {"pthread", "GL", "m", "dl", "rt", "X11"}
+
+	-- filter "system:macosx"
+	--     links {"OpenGL.framework", "Cocoa.framework", "IOKit.framework", "CoreFoundation.framework", "CoreAudio.framework", "CoreVideo.framework"}
+
+	filter{}
+end
+
+group "External dependecies"
+	project "glfw"
+		kind "StaticLib"
+		language "C"
+		-- staticruntime "off"
+		-- warnings "off"
+	
+		location ("../_project")
+		targetdir "../_bin/%{cfg.buildcfg}"
+		--targetdir ("_bin/" .. outputdir .. "/%{prj.name}")
+		--objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+		files
+		{
+			"premake5.lua",
+			"include/GLFW/glfw3.h",
+			"include/GLFW/glfw3native.h",
+			"src/glfw_config.h",
+			"src/context.c",
+			"src/init.c",
+			"src/input.c",
+			"src/monitor.c",
+	
+			"src/null_init.c",
+			"src/null_joystick.c",
+			"src/null_monitor.c",
+			"src/null_window.c",
+	
+			"src/platform.c",
+			"src/vulkan.c",
+			"src/window.c",
+		}
+	
+		filter "system:linux"
+			pic "On"
+	
+			systemversion "latest"
+			
+			files
+			{
+				"src/x11_init.c",
+				"src/x11_monitor.c",
+				"src/x11_window.c",
+				"src/xkb_unicode.c",
+				"src/posix_module.c",
+				"src/posix_time.c",
+				"src/posix_thread.c",
+				"src/posix_module.c",
+				"src/glx_context.c",
+				"src/egl_context.c",
+				"src/osmesa_context.c",
+				"src/linux_joystick.c"
+			}
+	
+			defines
+			{
+				"_GLFW_X11"
+			}
+	
+		filter "system:macosx"
+			pic "On"
+	
+			files
+			{
+				"src/cocoa_init.m",
+				"src/cocoa_monitor.m",
+				"src/cocoa_window.m",
+				"src/cocoa_joystick.m",
+				"src/cocoa_time.c",
+				"src/nsgl_context.m",
+				"src/posix_thread.c",
+				"src/posix_module.c",
+				"src/osmesa_context.c",
+				"src/egl_context.c"
+			}
+	
+			defines
+			{
+				"_GLFW_COCOA"
+			}
+	
+		filter "system:windows"
+			systemversion "latest"
+			staticruntime "On"
+	
+			files
+			{
+				"src/win32_init.c",
+				"src/win32_joystick.c",
+				"src/win32_module.c",
+				"src/win32_monitor.c",
+				"src/win32_time.c",
+				"src/win32_thread.c",
+				"src/win32_window.c",
+				"src/wgl_context.c",
+				"src/egl_context.c",
+				"src/osmesa_context.c"
+			}
+	
+			defines 
+			{ 
+				"_GLFW_WIN32",
+				"_CRT_SECURE_NO_WARNINGS"
+			}
+	
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+	
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "on"
+group "" -- End of dependencies
