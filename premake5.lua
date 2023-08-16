9da4df200f46ec734b32d4e2cd695a73100e103d
Anssi Lappalainen <anssilap@users.noreply.github.com>
2023-08-16 21:18:32 +0300

    add missing posix poll header for linux

--- premake5.lua
+++ premake5.lua
@@ -63,6 +63,7 @@ group "External dependecies"
 				"src/posix_time.c",
 				"src/posix_thread.c",
 				"src/posix_module.c",
+				"src/posix_poll.c",
 				"src/glx_context.c",
 				"src/egl_context.c",
 				"src/osmesa_context.c",
